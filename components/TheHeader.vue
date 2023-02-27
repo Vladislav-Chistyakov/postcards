b3a07d14a130f09b667f9f753852baf5375b1e83
Vladislav_Chistyakov <vladischis@gmail.com>
2023-02-27 09:56:55 +0300

    version 0.1.0

---
+++ components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="container">
+      <nav class="nav">
+        <h3>
+          Nuxt App
+        </h3>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style>
+  .container {
+    max-width: 1400px;
+    padding: 20px 40px;
+    margin: 0 auto;
+    color: white;
+    background-color: blue;
+  }
+</style>
